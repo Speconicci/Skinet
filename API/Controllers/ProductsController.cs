d07fa5959bd30b58fc1089cb48fd3ae28c640ed7
Ivan Bobok <speconium@outlook.com>
2023-02-27 22:44:02 +0200

    Initial Commit

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,43 @@
+﻿using Infrastructure.Data;
+using Core.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext context;
+
+        public ProductsController(StoreContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var products = await  context.Products.ToListAsync();
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            return await context.Products.FindAsync(id);
+        }
+
+        private void Insert3Rows()
+        {
+            context.Products.Add(new Product() { Id = 1, Name = "Product1" });
+            context.Products.Add(new Product() { Id = 2, Name = "Product2" });
+            context.Products.Add(new Product() { Id = 3, Name = "Product3" });
+            context.SaveChanges();
+        }
+    }
+}
